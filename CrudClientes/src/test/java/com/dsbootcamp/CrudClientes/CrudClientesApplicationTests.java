5ba740aa19c056c61c25693fc6af0975f5fc0caf
Renan Santos de Souza <renansantos-25855@hotmail.com>
2021-05-15 12:56:00 -0300

    CRUD Client

---
+++ CrudClientes/src/test/java/com/dsbootcamp/CrudClientes/CrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsbootcamp.CrudClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
